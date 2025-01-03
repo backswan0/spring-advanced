9eecab2023aa31366a4ae131b52948049d06a0cd
backswan0 <rnrwprh3434@gmail.com>
2025-01-04 00:25:27 +0900

    :art: refactor/level 3-4: N+1 Resolution
    resolve N+1 problem using entity graph annotations

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,13 +3,13 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 // 중복 코드 해결: default 메서드를 써서 해결해보자 (일정을 여러 군데에서 호출하지만, 관리는 여기서만 이루어지니까)
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-  @Query("SELECT t From Todo t join fetch t.user")
+  @EntityGraph(attributePaths = {"user"})
   Page<Todo> findAllByOrderByUpdatedAtDesc(Pageable pageable);
 
   int countById(Long todoId);
