2fcd92ba0c67fa4d19c75921734e7b8798e933bf
backswan0 <rnrwprh3434@gmail.com>
2025-01-03 20:04:29 +0900

    :art: refactor/level extra-auth-controller
    wrap Response DTO with ResponseEntity to follow team convention

--- src/main/java/org/example/expert/domain/auth/controller/AuthController.java
+++ src/main/java/org/example/expert/domain/auth/controller/AuthController.java
@@ -7,6 +7,8 @@ import org.example.expert.domain.auth.dto.request.SignUpRequestDto;
 import org.example.expert.domain.auth.dto.response.SignInResponseDto;
 import org.example.expert.domain.auth.dto.response.SignUpResponseDto;
 import org.example.expert.domain.auth.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,16 +22,20 @@ public class AuthController {
   private final AuthService authService;
 
   @PostMapping("/sign-up")
-  public SignUpResponseDto signUp(
+  public ResponseEntity<SignUpResponseDto> signUp(
       @Valid @RequestBody SignUpRequestDto requestDto
   ) {
-    return authService.signUp(requestDto);
+    SignUpResponseDto responseDto = authService.signUp(requestDto);
+
+    return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
   }
 
   @PostMapping("/sign-in")
-  public SignInResponseDto signIn(
+  public ResponseEntity<SignInResponseDto> signIn(
       @Valid @RequestBody SignInRequestDto requestDto
   ) {
-    return authService.signIn(requestDto);
+    SignInResponseDto responseDto = authService.signIn(requestDto);
+
+    return new ResponseEntity<>(responseDto, HttpStatus.OK);
   }
 }
