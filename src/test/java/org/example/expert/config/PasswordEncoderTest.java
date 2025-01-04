009c517832511952b58aba6e74b45fd77c3fd10c
backswan0 <rnrwprh3434@gmail.com>
2025-01-04 22:40:35 +0900

    :test_tube: test/level 5-1: test practice 1
    - change the parameter order of matches for successful test
    
    - add test to ensure matches method does not match incorrect password

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -1,5 +1,6 @@
 package org.example.expert.config;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -10,19 +11,37 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 class PasswordEncoderTest {
 
-    @InjectMocks
-    private PasswordEncoder passwordEncoder;
+  @InjectMocks
+  private PasswordEncoder passwordEncoder;
 
-    @Test
-    void matches_메서드가_정상적으로_동작한다() {
-        // given
-        String rawPassword = "testPassword";
-        String encodedPassword = passwordEncoder.encode(rawPassword);
+  @Test
+  void matches_메서드가_정상적으로_동작한다() {
+    // given
+    String rawPassword = "testPassword";
+    String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+    // when
+    boolean matches = passwordEncoder.matches(
+        rawPassword,
+        encodedPassword
+    );
 
-        // then
-        assertTrue(matches);
-    }
+    // then
+    assertTrue(matches);
+  }
+
+  @Test
+  void matches_메서드가_잘못된_비밀번호를_매칭하지_않는다() {
+    // given
+    String rawPassword = "testPassword";
+    String encodedPassword = passwordEncoder.encode(rawPassword);
+
+    // when
+    boolean mismatches = passwordEncoder.matches(
+        "wrongPassword",
+        encodedPassword
+    );
+
+    assertFalse(mismatches);
+  }
 }
